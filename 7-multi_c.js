8cb279247bafd96ad960734a9eadbc0cde04adec
ABOT-DK <davidkabotsi@gmail.com>
2025-05-24 17:48:18 +0000

    second updated 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,8 +3,9 @@ let x = parseInt(process.argv[2])
 if(x%1 != 0){
     console.log("Missing number of occurrences")
 } else {
-    Array(x).fill().forEach(() => {
-        console.log("C is fun");
+    let arr = new Array(x);
+    arr.fill(0).forEach(() => {
+      console.log("C is fun");
     })
 }
 
