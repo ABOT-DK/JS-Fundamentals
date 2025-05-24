5a9ed031f20ff402ff54bb1221400ecdfe682d74
ABOT-DK <davidkabotsi@gmail.com>
2025-05-24 17:54:00 +0000

    added 8-square.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,9 +3,8 @@ let x = parseInt(process.argv[2])
 if(x%1 != 0){
     console.log("Missing number of occurrences")
 } else {
-    let arr = new Array(x);
-    arr.fill(0).forEach(() => {
-      console.log("C is fun");
+    Array(x).fill().forEach(() => {
+        console.log("C is fun");
     })
 }
 
