b20d988689bba1821cf0cc23bf2f15e0539a4e8b
ABOT-DK <davidkabotsi@gmail.com>
2025-05-24 18:06:14 +0000

    update 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -5,7 +5,7 @@ function add(a, b) {
   let int1 = parseInt(process.argv[2]);
   let int2 = parseInt(process.argv[3]);
   
-  if (int1%1 != 0 || int2%1 != 0) {
+  if (isNaN(int1) || isNaN(int2)) {
     console.log("Invalid arguments - please provide two integers");
   } else {
     console.log(add(int1, int2));
