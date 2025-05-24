7a63943e0ce35dc7f62b598349a75f363ca7d68f
ABOT-DK <davidkabotsi@gmail.com>
2025-05-24 14:47:00 +0000

    added 1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,8 @@
+let firstStatement = "C is fun"
+let secondStatement = "Python is cool"
+let thirdStatement = "Javascript is amazing"
+
+console.log(firstStatement)
+console.log(secondStatement)
+console.log(thirdStatement)
+
