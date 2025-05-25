ba525ca3a17e2e33b035f49094a4730e21d7f0cb
ABOT-DK <davidkabotsi@gmail.com>
2025-05-25 22:14:26 +0000

    updated file 7

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,4 +1,4 @@
-let x = parseInt(process.argv[2])
+/*let x = parseInt(process.argv[2])
 
 if(x%1 != 0){
     console.log("Missing number of occurrences")
@@ -6,5 +6,14 @@ if(x%1 != 0){
     Array(x).fill().forEach(() => {
         console.log("C is fun");
     })
-}
+}*/
 
+let x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+    console.log("Missing number of occurrences");
+} else if (x > 0) {
+    for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
+}
